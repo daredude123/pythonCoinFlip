ffa4bcd3768ea24a4b71e4a9d8300c1c5d4737ce
andreas <andsek@hotmail.com>
2019-12-11 23:07:26 +0100

    Adding resources and cv2

--- coinFlipper.py
+++ coinFlipper.py
@@ -1,18 +1,28 @@
 import random
+import numpy as np
+import cv2
 
 print("Flip a coin")
 
 class Coin:
     side = "kron" #default side
+    mynt_side_image = cv2.imread("/home/andreas/python/pythonCoinFlip/resources/mynt.png")
+    kron_side_image = cv2.imread("/home/andreas/python/pythonCoinFlip/resources/kron.png")
+
     def printside(self):
         if self.side == 1:
-            return "kron"
+            self.side = "kron"
+            cv2.imshow('image',self.kron_side_image)
+            cv2.waitKey()
         else:
-            return "mynt"
+            self.side = "mynt"
+            cv2.imshow('image2',self.kron_side_image)
+            cv2.waitKey()
 
 def flipCoin(coin):
     coin.side = random.randint(1,2)
     print(coin.printside())
+
 
 coin = Coin()
 flipCoin(coin)
