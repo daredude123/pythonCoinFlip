b34e8999f3c22f8e015c4c4f7e6fe6674a0bde9f
andreas <andsek@hotmail.com>
2019-12-11 23:48:33 +0100

    testing

---
+++ test.py
@@ -0,0 +1,6 @@
+"""Testing with python"""
+
+def test_text():
+    """teeeest"""
+    assert 5 != 4, "not THE SAME"
+    
--- test/test.py
+++
@@ -1,9 +0,0 @@
-"""Testing with python"""
-import unittest
-
-class Test(unittest.TestCase):
-    """Test Class"""
-
-    def test_something(self):
-        """test for tests sake"""
-        self.assertEqual("test", "test")
