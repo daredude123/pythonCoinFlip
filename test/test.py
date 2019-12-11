5469326b2c6d4e0b70815d008322f01837f39682
andreas <andsek@hotmail.com>
2019-12-11 23:40:18 +0100

    adding tests for github flow

---
+++ test/test.py
@@ -0,0 +1,9 @@
+"""Testing with python"""
+import unittest
+
+class Test(unittest.TestCase):
+    """Test Class"""
+
+    def test_something(self):
+        """test for tests sake"""
+        self.assertEqual("test", "test")
